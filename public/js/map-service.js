f923691bce68f9711975cab67d503688dd12736c
kalish <uri.kalish@gmail.com>
2023-06-03 20:28:16 +0300

    map marker titles

--- public/js/map-service.js
+++ public/js/map-service.js
@@ -56,6 +56,14 @@ export class MapService {
 		await this.handleIssMapMarker(this.map, issCam, onMarkerClick);
 	}
 
+	getMarkerTitle(cam) {
+		let geo = `${cam.geo.split('/')[1]}, ${cam.geo.split('/')[0]}`;
+		const lat = Number.parseFloat(cam.pos.split(',')[0]).toFixed(5);
+		const lng = Number.parseFloat(cam.pos.split(',')[1]).toFixed(5);
+		const tags = cam.tags ? cam.tags.replaceAll(',', ', ') : '';
+		return `${cam.name}\n${geo}\n${lat}, ${lng}${tags ? '\n' + tags : ''}`;
+	}
+
 	async addLocationMarkers(map, cams, onMarkerClick) {
 		//@ts-ignore
 		const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary('marker');
@@ -70,7 +78,7 @@ export class MapService {
 			const marker = new AdvancedMarkerElement({
 				map,
 				position: {lat, lng},
-				title: `${cam.name}\n${cam.tags ? cam.tags + '\n' : ''}${cam.geo}\n${lat.toFixed(5)},${lng.toFixed(5)}`,
+				title: this.getMarkerTitle(cam),
 				content: this.getPin(PinElement, false),
 				//collisionBehavior: 'OPTIONAL_AND_HIDES_LOWER_PRIORITY'
 			});
@@ -94,6 +102,7 @@ export class MapService {
 			const json = await req.json();
 			const lat = Number(json.latitude);
 			const lng = Number(json.longitude);
+			issCam.pos = `${lat},${lng}`;
 			if (!this.issMarker) {
 				const svgMarker = {
 					path: 'm15.44.59-3.18 3.18c-.78.78-.78 2.05 0 2.83l1.24 1.24-.71.71-1.24-1.25c-.78-.78-2.05-.78-2.83 0L7.3 8.72c-.78.78-.78 2.05 0 2.83l1.24 1.24-.71.71-1.23-1.25c-.78-.78-2.05-.78-2.83 0L.59 15.43c-.78.78-.78 2.05 0 2.83l3.54 3.54c.78.78 2.05.78 2.83 0l3.18-3.18c.78-.78.78-2.05 0-2.83L8.9 14.55l.71-.71 1.24 1.24c.78.78 2.05.78 2.83 0l1.41-1.41c.78-.78.78-2.05 0-2.83L13.84 9.6l.71-.71 1.24 1.24c.78.78 2.05.78 2.83 0l3.18-3.18c.78-.78.78-2.05 0-2.83L18.26.58c-.78-.78-2.04-.78-2.82.01zM6.6 19.32l-1.06 1.06L2 16.85l1.06-1.06 3.54 3.53zm2.12-2.12-1.06 1.06-3.54-3.54 1.06-1.06 3.54 3.54zm9.54-9.54L17.2 8.72l-3.54-3.54 1.06-1.06 3.54 3.54zm2.12-2.12L19.32 6.6l-3.54-3.54L16.85 2l3.53 3.54zM14 21v2c4.97 0 9-4.03 9-9h-2c0 3.87-3.13 7-7 7zm0-4v2c2.76 0 5-2.24 5-5h-2c0 1.66-1.34 3-3 3z',
@@ -107,7 +116,7 @@ export class MapService {
 				this.issMarker = new google.maps.Marker({
 					map,
 					position: {lat, lng},
-					title: `${issCam.name}\n${issCam.tags}\n${issCam.geo}\n${lat.toFixed(5)},${lng.toFixed(5)}`,
+					title: this.getMarkerTitle(issCam),
 					icon: svgMarker,
 				});
 				google.maps.event.addListener(this.issMarker, 'click', () => {
