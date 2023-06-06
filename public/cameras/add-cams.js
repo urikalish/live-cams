b48aabbc7e41181dbbdbec334ea92c330cfe8754
kalish <uri.kalish@gmail.com>
2023-06-06 23:29:21 +0300

    add cams

--- public/cameras/add-cams.js
+++ public/cameras/add-cams.js
@@ -1,10 +1,3 @@
 export const addCams =
 [
-	{
-		"name": "Holon city Webcam 2",
-		"tags": "City,Traffic",
-		"geo": "Israel/Holon",
-		"pos": "32.015994862538804,34.78738788966109",
-		"src": "https://www.youtube.com/embed/QPtKUmXKY2o"
-	},
 ]
