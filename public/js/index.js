e6bca8b5dc05c7c3e621a4db488b8b2e858bd660
kalish <uri.kalish@gmail.com>
2023-06-18 11:28:48 +0300

    query params

--- public/js/index.js
+++ public/js/index.js
@@ -7,27 +7,24 @@ import { issCam } from '../cameras/iss-cam.js';
 import { MapService} from './map-service.js';
 import { CamService} from './cam-service.js';
 
-const displayAllErrCams = false;
-const autoShowIssCam = true;
-const autoPlay = true;
-const closestCount = 16;
-
 const mapService = new MapService();
 const camService = new CamService();
 
-function activateCamsForMarkers(markers) {
-	camService.displayLiveCams(markers.map(m => m.cam), autoPlay);
-}
+const urlParams = new URLSearchParams(window.location.search);
+const mode = urlParams.get('mode') || 'view';
 
-window.handleGoogleMapLoaded = () => {
-	mapService.init(camService.getCams(), issCam, closestCount, activateCamsForMarkers).then(()=>{});
-}
-
-if (displayAllErrCams) {
-	camService.displayAllErrCams(wctCams, errCams, remCams, addCams, updCams);
-} else {
+if (mode === 'view') {
+	const autoShowIssCam = true;
+	const autoPlay = true;
+	const closestCount = 16;
+	window.handleGoogleMapLoaded = () => {
+		mapService.init(camService.getCams(), issCam, closestCount, markers => {camService.displayLiveCams(markers.map(m => m.cam), autoPlay);}).then(()=>{});
+	}
 	camService.init(wctCams, errCams, remCams, addCams, updCams);
 	if (autoShowIssCam) {
 		camService.displayLiveCams([issCam], autoPlay);
 	}
+} else if (mode === 'check') {
+	window.handleGoogleMapLoaded = () => {};
+	camService.displayAllErrCams(wctCams, errCams, remCams, addCams, updCams);
 }
